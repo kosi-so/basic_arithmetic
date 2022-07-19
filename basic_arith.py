e880dc37d92ee748172d0d2d5bf25870c9a2ba4b
Kosisochukwu Akaeze <55185507+kosi-so@users.noreply.github.com>
2022-07-19 17:38:40 +1000

    Add files via upload

---
+++ basic_arith.py
@@ -0,0 +1,29 @@
+
+def add (x, y):
+    """Add Function"""
+    return x + y
+
+
+def subtract (x, y):
+    """Subtract Function"""
+    pass
+
+
+def multiply (x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide (x, y):
+    """Divide Function"""
+    return x / y
+
+
+def floor (x, y):
+    """Floor Fuction"""
+    pass
+
+
+def power (x, y):
+    """Divide Function"""
+    return x ** y
